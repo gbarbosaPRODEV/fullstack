86e51b7886fd025f9ac20bc811c4fb678b388e3e
gbarbosaPRODEV <guilhermehenriquecba@gmail.com>
2025-03-31 13:44:07 -0300

    Add files via upload

---
+++ jogo.js
@@ -0,0 +1,24 @@
+let randomNumber = Math.floor(Math.random() * 100);
+
+function checkGuess() {
+    let userGuess = parseInt(document.getElementById("guessInput").value);
+    let message = document.getElementById("message");
+
+    if (isNaN(userGuess) || userGuess < 0 || userGuess > 99) {
+        message.textContent = "Por favor, insira um número válido entre 0 e 99.";
+        message.style.color = "yellow";
+        return;
+    }
+
+    if (userGuess === randomNumber) {
+        message.textContent = "Parabéns! Você acertou o número!";
+        message.style.color = "green";
+        document.body.style.backgroundColor = "green";
+    } else if (userGuess < randomNumber) {
+        message.textContent = "O número é maior! Tente novamente.";
+        message.style.color = "red";
+    } else {
+        message.textContent = "O número é menor! Tente novamente.";
+        message.style.color = "red";
+    }
+}
